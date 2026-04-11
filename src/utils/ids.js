5a4340c1ce63740ce793b614da2f1dea230aae04
gautamnaik23 <gautamabhangnaik@gmail.com>
2026-04-11 11:07:45 -0700

    Update ids.js

--- src/utils/ids.js
+++ src/utils/ids.js
@@ -7,5 +7,5 @@ export function generateSessionId() {
   const month = String(now.getMonth() + 1).padStart(2, '0');
   const day = String(now.getDate()).padStart(2, '0');
   const year = String(now.getFullYear()).slice(-2);
-  return `${month}${day}${year}`;
+  return `${year}${month}${day}`;
 }
